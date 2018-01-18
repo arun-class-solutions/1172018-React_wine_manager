73b71577648d7b4fdb58e32fea335e0eade1721c
Arun Sood <arsood@gmail.com>
2018-01-18 10:55:03 -0800

    Configure routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, Switch } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 
 import WineList from "./WineList";
 
 import "./style.css";
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <WineList />,
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/wines" component={WineList} />
+    </Switch>
+  </Router>,
   document.getElementById("root")
 );
